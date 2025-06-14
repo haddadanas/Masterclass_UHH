7def987117a9bc3afcfc96b1dcdf71a5594b6df6
Anas Haddad <hhhaaanas@gmail.com>
2025-06-14 23:55:54 +0200

    refactor: remove unused stats div and reorganize button event listeners

--- ts/ispy.ts
+++ ts/ispy.ts
@@ -48,86 +48,68 @@ document.addEventListener("DOMContentLoaded", () => {
   run();
 
   console.log(event_description);
-});
 
-// connect functions to the buttons
-document.getElementById("js-reload")?.addEventListener("click", reload);
-document.getElementById("js-prev-event-button")?.addEventListener("click", prevEvent);
-document.getElementById("js-next-event-button")?.addEventListener("click", nextEvent);
-document.getElementById("js-reset-view")?.addEventListener("click", resetView);
-document.getElementById("js-zoom-in")?.addEventListener("click", zoomIn);
-document.getElementById("js-zoom-out")?.addEventListener("click", zoomOut);
-document.getElementById("js-autorotate")?.addEventListener("click", autoRotate);
-document.getElementById("3d")?.addEventListener("click", () => {
-  showView("3D");
-});
-document.getElementById("rphi")?.addEventListener("click", () => {
-  showView("RPhi");
-});
-document.getElementById("rhoz")?.addEventListener("click", () => {
-  showView("RhoZ");
-});
-document.getElementById("xy")?.addEventListener("click", setXY);
-document.getElementById("yz")?.addEventListener("click", setYZ);
-document.getElementById("xz")?.addEventListener("click", setZX);
-document.getElementById("perspective")?.addEventListener("click", setPerspective);
-document.getElementById("orthographic")?.addEventListener("click", setOrthographic);
-document.getElementById("enterFullscreen")?.addEventListener("click", enterFullscreen);
-document.getElementById("exitFullscreen")?.addEventListener("click", exitFullscreen);
-document.getElementById("js-render-info")?.addEventListener("click", updateRendererInfo);
-document.getElementById("js-analysis-btn")?.addEventListener("click", getSelectionResults);
-document.getElementById("prev-sel-event")?.addEventListener("click", prevSelectedEvent);
-document.getElementById("next-sel-event")?.addEventListener("click", nextSelectedEvent);
-document.getElementById("js-print-btn")?.addEventListener("click", printImage);
-document.getElementById("animate")?.addEventListener("click", toggleAnimation);
-document.getElementById("js-show-webfiles-btn")?.addEventListener("click", showWebFiles);
-document.getElementById("js-local-files-btn")?.addEventListener("change", loadLocalFiles);
-document.getElementById("import-file")?.addEventListener("change", importModel);
-document.getElementById("js-import-web")?.addEventListener("click", () => {
-  openDialog("#geometry-files");
-  loadObjFiles();
-  $("#import-model").modal("hide");
-});
-document.getElementById("invert-colors")?.addEventListener("change", invertColors);
-document.getElementById("vh-slider")?.addEventListener("input", (event) => {
-  const value = parseFloat((event.target as HTMLInputElement).value);
-  setDisplayVerticalHeight(value);
-});
-document.getElementById("fps-slider")?.addEventListener("input", (event) => {
-  const value = parseFloat((event.target as HTMLInputElement).value);
-  setFramerate(value);
-});
-document.getElementById("transparency-slider")?.addEventListener("input", (event) => {
-  const value = parseFloat((event.target as HTMLInputElement).value);
-  setTransparency(value);
-});
-document.getElementById("js-webgl-renderer")?.addEventListener("click", () => {
-  updateRenderer("WebGLRenderer");
-});
-document.getElementById("js-svg-renderer")?.addEventListener("click", () => {
-  updateRenderer("SVGRenderer");
-});
-document.getElementById("js-export-obj")?.addEventListener("click", exportOBJ);
-document.getElementById("js-export-gltf-text")?.addEventListener("click", exportGLTF_text);
-document.getElementById("js-export-gltf-binary")?.addEventListener("click", exportGLTF_binary);
-document.getElementById("load-obj")?.addEventListener("click", () => {
-  $("#geometry-files").modal("hide");
-  loadSelectedObj();
-});
-document.getElementById("load-event")?.addEventListener("click", () => {
-  $("#files").modal("hide");
-  buildFileSummary();
-  loadEvent();
-});
-document.getElementById("js-csv-higgs")?.addEventListener("click", () => {
-  createCSV("Higgs");
-});
-document.getElementById("js-csv-z")?.addEventListener("click", () => {
-  createCSV("Z");
-});
-document.getElementById("js-csv-wp")?.addEventListener("click", () => {
-  createCSV("Wp");
-});
-document.getElementById("js-csv-wm")?.addEventListener("click", () => {
-  createCSV("Wm");
+  // connect functions to the buttons
+  document.getElementById("js-reload")?.addEventListener("click", reload);
+  document.getElementById("js-prev-event-button")?.addEventListener("click", prevEvent);
+  document.getElementById("js-next-event-button")?.addEventListener("click", nextEvent);
+  document.getElementById("js-reset-view")?.addEventListener("click", resetView);
+  document.getElementById("js-zoom-in")?.addEventListener("click", zoomIn);
+  document.getElementById("js-zoom-out")?.addEventListener("click", zoomOut);
+  document.getElementById("js-autorotate")?.addEventListener("click", autoRotate);
+  document.getElementById("3d")?.addEventListener("click", () => showView("3D"));
+  document.getElementById("rphi")?.addEventListener("click", () => showView("RPhi"));
+  document.getElementById("rhoz")?.addEventListener("click", () => showView("RhoZ"));
+  document.getElementById("xy")?.addEventListener("click", setXY);
+  document.getElementById("yz")?.addEventListener("click", setYZ);
+  document.getElementById("xz")?.addEventListener("click", setZX);
+  document.getElementById("perspective")?.addEventListener("click", setPerspective);
+  document.getElementById("orthographic")?.addEventListener("click", setOrthographic);
+  document.getElementById("enterFullscreen")?.addEventListener("click", enterFullscreen);
+  document.getElementById("exitFullscreen")?.addEventListener("click", exitFullscreen);
+  document.getElementById("js-render-info")?.addEventListener("click", updateRendererInfo);
+  document.getElementById("js-analysis-btn")?.addEventListener("click", getSelectionResults);
+  document.getElementById("prev-sel-event")?.addEventListener("click", prevSelectedEvent);
+  document.getElementById("next-sel-event")?.addEventListener("click", nextSelectedEvent);
+  document.getElementById("js-print-btn")?.addEventListener("click", printImage);
+  document.getElementById("animate")?.addEventListener("click", toggleAnimation);
+  document.getElementById("js-show-webfiles-btn")?.addEventListener("click", showWebFiles);
+  document.getElementById("js-local-files-btn")?.addEventListener("change", loadLocalFiles);
+  document.getElementById("import-file")?.addEventListener("change", importModel);
+  document.getElementById("js-import-web")?.addEventListener("click", () => {
+    openDialog("#geometry-files");
+    loadObjFiles();
+    $("#import-model").modal("hide");
+  });
+  document.getElementById("invert-colors")?.addEventListener("change", invertColors);
+  document.getElementById("vh-slider")?.addEventListener("input", (event) => {
+    const value = parseFloat((event.target as HTMLInputElement).value);
+    setDisplayVerticalHeight(value);
+  });
+  document.getElementById("fps-slider")?.addEventListener("input", (event) => {
+    const value = parseFloat((event.target as HTMLInputElement).value);
+    setFramerate(value);
+  });
+  document.getElementById("transparency-slider")?.addEventListener("input", (event) => {
+    const value = parseFloat((event.target as HTMLInputElement).value);
+    setTransparency(value);
+  });
+  document.getElementById("js-webgl-renderer")?.addEventListener("click", () => updateRenderer("WebGLRenderer"));
+  document.getElementById("js-svg-renderer")?.addEventListener("click", () => updateRenderer("SVGRenderer"));
+  document.getElementById("js-export-obj")?.addEventListener("click", exportOBJ);
+  document.getElementById("js-export-gltf-text")?.addEventListener("click", exportGLTF_text);
+  document.getElementById("js-export-gltf-binary")?.addEventListener("click", exportGLTF_binary);
+  document.getElementById("load-obj")?.addEventListener("click", () => {
+    $("#geometry-files").modal("hide");
+    loadSelectedObj();
+  });
+  document.getElementById("load-event")?.addEventListener("click", () => {
+    $("#files").modal("hide");
+    buildFileSummary();
+    loadEvent();
+  });
+  document.getElementById("js-csv-higgs")?.addEventListener("click", () => createCSV("Higgs"));
+  document.getElementById("js-csv-z")?.addEventListener("click", () => createCSV("Z"));
+  document.getElementById("js-csv-wp")?.addEventListener("click", () => createCSV("Wp"));
+  document.getElementById("js-csv-wm")?.addEventListener("click", () => createCSV("Wm"));
 });
