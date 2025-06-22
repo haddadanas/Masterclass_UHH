bb669e1864db90dca1d5c2379a56b66c4a504474
Anas Haddad <hhhaaanas@gmail.com>
2025-06-22 14:23:57 +0200

    adjusted the configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./ts/ispy.ts", // Your main TypeScript entry point
+  // Dynamically set entry points for all .ts files in the ts folder
+  entry: "./dist/ispy.js",
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader"],
-      },
-    ],
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.tsx?$/,
+  //       use: "ts-loader",
+  //       exclude: /node_modules/,
+  //     },
+  //     {
+  //       test: /\.css$/i,
+  //       use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader"],
+  //     },
+  //   ],
+  // },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Path to your HTML file
+      inject: 'head' // Inject scripts into the <head>
+    }),
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -33,44 +39,43 @@ module.exports = {
           from: "node_modules/stupid-table-plugin/stupidtable.min.js",
           to: "lib/stupidtable.min.js",
         },
+        {
+          from: "node_modules/jquery.scrollintoview/jquery.scrollintoview.js",
+          to: "lib/jquery.scrollintoview.js",
+        },
       ],
     }),
   ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "libs",
-          chunks: "all",
-        },
-        common: {
-          test: /[\\/]ts[\\/]/,
-          name: "main",
-          chunks: "all",
-        },
-      },
-    },
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    fallback: {
-      buffer: false, // Disable buffer polyfill
-      stream: false, // Disable stream polyfill
-      assert: false, // Disable assert polyfill
-    },
-  },
+  // optimization: {
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendors: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: "libs",
+  //         chunks: "all",
+  //       },
+  //     },
+  //   },
+  // },
+  // resolve: {
+  //   extensions: [".tsx", ".ts", ".js"],
+  //   fallback: {
+  //     buffer: false,
+  //     stream: false,
+  //     assert: false,
+  //   },
+  // },
   output: {
-    filename: "masterclass_[name].js",
+    filename: "masterclass_[name].js", // Output each file with its name
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: "source-map", // Enable source maps for easier debugging
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 8080,
-    hot: true, // Enable Hot Module Replacement
-  },
+  // devtool: "source-map",
+  // devServer: {
+  //   static: {
+  //     directory: path.join(__dirname, "dist"),
+  //   },
+  //   compress: true,
+  //   port: 8080,
+  //   hot: true,
+  // },
 };
