2fcb90ad131f66351cbe50434fd27bdf736e3c64
Anas Haddad <hhhaaanas@gmail.com>
2025-10-15 13:32:00 +0200

    working modular version

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,53 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import path from "path";
 
-module.exports = {
-  // Dynamically set entry points for all .ts files in the ts folder
-  entry: "./js/ispy.js",
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html', // Path to your HTML file
-      inject: 'head', // Inject scripts into the <head>
-      filename: '../../index.html', // Output file in the dist folder
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "node_modules/jquery/dist/jquery.min.js",
-          to: "jquery.min.js",
-        },
-        {
-          from: "node_modules/stupid-table-plugin/stupidtable.min.js",
-          to: "stupidtable.min.js",
-        },
-        {
-          from: "node_modules/jquery.scrollintoview/jquery.scrollintoview.js",
-          to: "jquery.scrollintoview.js",
-        },
-      ],
-    }),
-  ],
-  output: {
-    filename: "masterclass_[name].js", // Output each file with its name
-    path: path.resolve(__dirname, "dist/assets/js/"),
-  },
-  // devtool: "source-map",
-  // devServer: {
-  //   static: {
-  //     directory: path.join(__dirname, "dist"),
-  //   },
-  //   compress: true,
-  //   port: 8080,
-  //   hot: true,
-  // },
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+
+export default async (env, options) => {
+  const dev = options.mode === "development";
+  const config = {
+    optimization: {
+      minimize: false,
+    },
+    devtool: dev ? "source-map" : false,
+    entry: "./js/ispy.js",
+    target: ["web", "es5"],
+    output: {
+      filename: "masterclass_[name].js", // Output each file with its name
+      path: path.resolve("dist/assets/js/"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "../../index.html",
+        template: "./index.html",
+        inject: "body",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "node_modules/jquery/dist/jquery.min.js",
+            to: "jquery.min.js",
+          },
+          {
+            from: "node_modules/stupid-table-plugin/stupidtable.min.js",
+            to: "stupidtable.min.js",
+          },
+          {
+            from: "node_modules/jquery.scrollintoview/jquery.scrollintoview.js",
+            to: "jquery.scrollintoview.js",
+          },
+        ],
+      }),
+      new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(!dev),
+      }),
+    ],
+  };
+
+  return config;
 };
